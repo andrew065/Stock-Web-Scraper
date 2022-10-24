f3baec3cf98ff2a604e6e8541f3d2f3c3ebcbbcb
andrewlian <andrewlian345@gmail.com>
2022-10-24 07:04:09 -0400

    Added program to download historical stock prices

--- IndustryStockModel.py
+++ IndustryStockModel.py
@@ -1,25 +1,61 @@
 import selenium.common.exceptions
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-
 import time
+from openpyxl import load_workbook
+
+
+def download_stock_info(name, ticker):
+    url = 'https://www.macrotrends.net/stocks/charts/%s/%s/stock-price-history'% (ticker, name)
+    driver.get(url)
+    driver.switch_to.frame('chart_iframe')
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    scripts = soup.findAll("script")
+    script = scripts[-1].string
+    link = script[script.find('window.parent.location.href') + 26:
+                  script.find('$(".imageExport").click( function()')].split('\'')[1]
+    driver.get(link)
+    time.sleep(0.1)
+
+
+def format_stock_name(stock_name):
+    words = stock_name.lower().split(' ')
+    final_name = ''
+    for word in words:
+        if word not in ['co', 'inc', 'ltd', 'corp', '&', 'group', 'holdings', 'SA']:
+            if final_name == '':
+                final_name += word
+            else:
+                final_name += '-' + word
+    return final_name
+
+
+def get_company_info():
+    stock_spreadsheet = load_workbook(filename='Spreadsheets/Wharton Stock List.xlsx')
+    industries = stock_spreadsheet.sheetnames
+
+    all_stocks = {}
+    for industry in industries[1:]:
+        stocks = []
+        for row in stock_spreadsheet[industry].iter_rows(min_row=2, max_col=2):
+            stock = []
+            for cell in row:
+                stock.append(cell.value)
+            stocks.append(stock)
+        all_stocks[industry] = stocks
+
+    return all_stocks
+
 
 driver = webdriver.Chrome()
-driver.get('https://www.macrotrends.net/stocks/charts/CVS/cvs-health/stock-price-history')
+industry_stocks = get_company_info()
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
-
-try:
-    elements = driver.find_element(By.ID, "main_content")
-
-    for element in elements.find_elements(By.ID, 'chart_container col-xs-12'):
-        print(element.get_attribute('innerHTML'))
-
-    print('\nbreak\n', elements.get_attribute('innerHTML'))
-
-except selenium.common.exceptions.NoSuchCookieException:
-    print("Button click unsuccessful")
-    driver.close()
+for industry in industry_stocks:
+    print('\n', industry)
+    for stock in industry_stocks[industry]:
+        print(format_stock_name(stock[0]), stock[1])
+        # download_stock_info(format_stock_name(stock[0]), stock[1])
 
 driver.close()
